a44a2a079769be0e310874aec158412513177cc9
Sang Nguyen <ngcongsang@gmail.com>
2022-04-03 09:26:33 +0700

    correct class name

--- src/test/java/builder/PurchaseTest.java
+++ src/test/java/builder/PurchaseTest.java
@@ -3,35 +3,14 @@ package builder;
 import org.testng.annotations.Test;
 import template.TestTemplate;
 
-public class PurchasePageTest extends TestTemplate {
+public class PurchaseTest extends TestTemplate {
     private PurchasePage purchasePage;
 
-    BillingInfor billingWithRequireFields = new BillingInfor.BillingInformationBuilder(
-            "sang",
-            "123 example.com",
-            "hcm")
-            .build();
+    BillingInfor billingWithRequireFields = new BillingInfor.BillingInformationBuilder("sang", "123 example.com", "hcm").build();
 
-    BillingInfor billingWithOptionalFields = new BillingInfor.BillingInformationBuilder(
-            "sang",
-            "123 example.com",
-            "hcm")
-            .state("hcm")
-            .zipCode("700000")
-            .build();
+    BillingInfor billingWithOptionalFields = new BillingInfor.BillingInformationBuilder("sang", "123 example.com", "hcm").state("hcm").zipCode("700000").build();
 
-    BillingInfor billingWithAllFields = new BillingInfor.BillingInformationBuilder(
-            "sang",
-            "123 example.com",
-            "hcm")
-            .state("hcm")
-            .zipCode("700000")
-            .cardType("American Express")
-            .cardNumber("2313156439")
-            .cardMonth("11")
-            .cardYear("2020")
-            .nameOnCard("SANG C NGUYEN")
-            .build();
+    BillingInfor billingWithAllFields = new BillingInfor.BillingInformationBuilder("sang", "123 example.com", "hcm").state("hcm").zipCode("700000").cardType("American Express").cardNumber("2313156439").cardMonth("11").cardYear("2020").nameOnCard("SANG C NGUYEN").build();
 
     @Test
     public void verifyPurchaseWithRequireFields() {
